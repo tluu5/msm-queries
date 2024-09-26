0d46183506fc5775b7ac15b799342b3590d076c4
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-09-26 21:53:15 +0000

    typo fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
   get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
   get("/directors/:the_id", { :controller => "movies", :action => "show" })
 end
