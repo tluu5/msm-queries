b7af61a6c0d53b5e76bbe1d9376f3038512c73fe
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-09-26 21:59:55 +0000

    routes on young and old

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/youngest", { :controller => "directors", :action => "junior" })
+  get("/directors/eldest", { :controller => "directors", :action => "senior" })
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:the_id", { :controller => "actors", :action => "show" })
